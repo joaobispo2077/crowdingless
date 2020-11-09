ed2235ec015130baac8ead693136033d257d7a84
João Bispo <vitor.vi10@hotmail.com>
2020-11-09 09:46:47 -0300

    :rocket: feat: image compressor

--- optimize.js
+++ optimize.js
@@ -1,16 +1,42 @@
 'use strict';
 
-module.exports.handle = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
+const sharp = require('sharp');
+
+const { basename, extname } = require('path');
+const S3 = new AWS.S3();
+
+module.exports.handle = async ({ Records: records }) => {
+  try {
+    await Promise.all(records.map(async record => {
+      const { key } = record.s3.object;
+
+      const image = await S3.getObject({
+        Bucket: process.env.bucket,
+        Key: key
+      }).promise();
+
+      const optimized = await sharp(image.Body)
+        .resize(1280, 720, { fiat: 'inside', withoutEnlargement: true })
+        .toFormat('jpeg', { progressive: true, quality: 50 })
+        .toBuffer();
+
+      await s3.putObject({
+        Body: optimized,
+        Bucket: process.env.bucket,
+        ContentType: 'image/jpeg',
+        Key: `compressed/${basename(key, extname(key))}.jpg`
+      }).promise()
+
+    }));
+
+    return {
+      statusCode: 201,
+      body: {}
+    }
+
+  } catch (err) {
+    return err;
+  }
 
 };
