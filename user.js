c4d6e29b659d5e45b946d6d4f4910d3bab376783
Joao Bispo <joaobispo2077@gmail.com>
2020-11-10 17:50:32 -0300

    :paperclip: chore: prepared REST lambda functions

---
+++ user.js
@@ -0,0 +1,20 @@
+
+const AWS = require('aws-sdk');
+
+module.exports = {
+  create: async(event, context) => {
+    
+  },
+  list: async(event, context) => {
+    
+  },  
+  get: async(event, context) => {
+    
+  },
+   update: async(event, context) => {
+    
+  },
+  delete: async(event, context) => {
+    
+  }
+}
