866482c718fce094fd88914444ddaacc5a84ddaa
João Bispo <vitor.vi10@hotmail.com>
2020-11-16 15:42:14 -0300

    :rocket: feat: created employee repositories that creat and read an employee

---
+++ src/repositories/employeeRepositories.js
@@ -0,0 +1,19 @@
+const employeeModel = require('../models/employeeModel');
+
+
+module.exports = {
+  async create(data) {
+    const employeeCreated = await employeeModel.put(data);
+    return employeeCreated;
+  },
+
+  async listAll() {
+    const employees = await employeeModel.scan();
+    return employees;
+  },
+
+  async getById(id) {
+    const employee = await employeeModel.get(id).promise();
+    return employee;
+  }
+}
