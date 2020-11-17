405adbaf5b6a1f5060e82c4d84ab7683343f3161
João Bispo <vitor.vi10@hotmail.com>
2020-11-17 16:29:12 -0300

    :paperclip: chore: add morgan to logs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const employeeModels = require('./models/employeeModel');
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(morgan('dev'));
 
 app.use('/employees', employeeRoutes);
 
